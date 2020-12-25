e72fa3757c275eb8d579ac1729e98da35437e7cd
dimitris <dimitris-papaconstantinou@softwareimaging.com>
2020-12-25 00:09:02 +0000

    Interpreter Framework

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
 
 namespace LoxNet
 {
   class Program
   {
+    static bool hadError = false;
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      if (args.Length > 1)
+      {
+        Console.WriteLine("Usage LoxNet [script]");
+      }
+      else if (args.Length == 1)
+      {
+        runFile(args[0]);
+      }
+      else
+      {
+        runPrompt();
+      }
     }
+
+    private static void runFile(string path)
+    {
+      string s = File.ReadAllText(path);
+      run(s);
+
+        // Indicate an error in the exit code.
+      if (hadError)
+        Environment.Exit(64);
+    }
+
+    private static void runPrompt()
+    {
+      for (;;)
+      {
+        Console.Write("> ");
+        string line = Console.ReadLine();
+
+        if (line == null)
+          break;
+
+        run(line);
+      }
+    }
+
+
+    private static void run(string source)
+    {
+      var tokens = source.Split(" ");
+
+      foreach (var token in tokens)
+      {
+        Console.WriteLine(token);
+      }
+    }
+
+    static void error(int line, string message)
+    {
+      report(line, "", message);
+    }
+
+    private static void report(int line, string where, string message)
+    {
+      Console.Error.WriteLine("[line " + line + "] Error" + where + ": " + message);
+      hadError = true;
+    }
+
   }
+
 }
